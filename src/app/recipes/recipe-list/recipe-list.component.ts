d279fbf5db64a2bfacc15f22a6003e1c7f26a4fe
Eduard Iacob <eduardiacob@Mac-mini-di-Armando.local>
2022-01-31 16:16:00 +0100

    Initial commit.

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is simply a test',
+      'https://blog.giallozafferano.it/allacciateilgrembiule/wp-content/uploads/2020/08/pasta-alla-marescialla3.jpg'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
