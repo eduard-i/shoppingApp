a896de5f5372558306364e3efe957792df240aaf
Eduard Iacob <eduardiacob@Mac-mini-di-Armando.local>
2022-02-02 11:05:54 +0100

    Feature: added the dropdown menu with a directive.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { DropdownDirective } from './shared/dropdown.directive';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header/header.component';
 import { NgModule } from '@angular/core';
@@ -10,7 +11,6 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,6 +21,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
+    DropdownDirective,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
